87f532e00c827f3ca2929c121ea6b67ad5a5d7fc
brqx <r23@mailz.es>
2026-03-02 13:29:15 +0100

    Starting App

---
+++ api/index_min.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// Captura ABSOLUTAMENTE TODO (*)
+app.all('*', (req, res) => {
+  res.json({
+    estado: "Node.js funcionando en Vercel Pro",
+    url_recibida: req.url,
+    metodo: req.method,
+    nota: "Si ves esto, el túnel está abierto"
+  });
+});
+
+module.exports = app;
