55f59e61e68642368b52e470897615b441bfe86f
brqx <r23@mailz.es>
2026-03-04 10:33:28 +0100

    Changes

--- api/index.js
+++ api/index.js
@@ -18,7 +18,8 @@ if (typeof global.define !== 'function') {
     };
 }
 
-const { params }          =   require(path.join(global.ROOT , 'api/apps/es7/com/ctyp/t01/params/p01_params.es7')  );
+// Checking long paths
+// const { params }          =   require(path.join(global.ROOT , 'api/apps/es7/com/ctyp/t01/params/p01_params.es7')  );
 
 
 //Constantes GLOBALES
