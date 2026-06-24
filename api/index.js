53171a49e3924b868adc3bb3a95c907930a5b0e2
brqx <r23@mailz.es>
2026-06-24 14:30:16 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -65,9 +65,10 @@ define("JS_TYF7" , 	path.join(global.ROOT, 'api/apps/es7/com/ctyp/t02/'			))
 
 // 3. IMPORTANTE: Cargamos tu lógica compleja (fire.es7)
 // Ajusta la ruta para que apunte a donde hayas subido el archivo en GitHub
-const fireApp = require('./apps/fire.js');
 
 const app = express();
+
+const fireApp = require('./apps/fire.js');
 
 //-------------------------------------------------------------------------------------
 let run_path                =   path.resolve(".")
